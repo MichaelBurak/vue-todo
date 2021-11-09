db9bfc9d48cb6c7d50ba1d0b8d1d0a32850f08d5
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-11-09 10:25:59 -0700

    init commit of basic todo

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>ToDos</h1>
+  <div v-for="todo in list" :key="todo.id" class="list">
+    {{ todo.content }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  data() { 
+      return {
+          list: [{
+              id: null,
+              title: ""
+          }]
+    }
+      },
+      mounted(){
+          fetch("http://127.0.0.1:8000/")
+          .then(res => res.json())
+          .then(data => this.list = data)
+          .catch(err => console.log(err))
+      }  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* just a bit of brutalism  */
+@font-face {
+  font-family: IBMPlexMono-Light;
+  src: url("../assets/IBM_Plex_Mono/IBMPlexMono-Light.ttf");
+}
+
+div {
+  font-family: IBMPlexMono-Light;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
